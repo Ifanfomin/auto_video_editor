4698049918a0c548393a857d42e60a1bcb6c6be9
ifanfomin <ifanfomin@rambler.ru>
2024-08-05 22:42:44 +0300

    Made script for export audio to video and save them

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.make_video import MakeVideo
 
 
 def main():
-    MakeVideo("media/video.mp3", "media/audio.mp3")
+    MakeVideo("media/Запись экрана от 2024-08-05 21-38-38.webm", "media/audio_2024-08-05_21-42-10.ogg")
 
 
 if __name__ == '__main__':
--- src/make_video.py
+++ src/make_video.py
@@ -1,5 +1,26 @@
-class MakeVideo:
-    def __init__(self, video_path, audio_path):
-        self.video_path = video_path
-        self.audio_path = audio_path
+from moviepy.editor import *
+import os
+from datetime import datetime
 
+
+class MakeVideo:
+    def __init__(
+            self,
+            video_path,
+            audio_path,
+            save_path="created_videos",
+            save_name=datetime.today().strftime("%Y-%m-%d-%H_%M_%S") + ".mp4"
+    ):
+        self.save_name = save_name
+        self.save_path = save_path
+        self.video_clip = VideoFileClip(video_path)
+        self.audio_clip = AudioFileClip(audio_path)
+
+        self.export_audio_to_video()
+        self.save_video()
+
+    def export_audio_to_video(self):
+        self.video_clip = self.video_clip.set_audio(self.audio_clip)
+
+    def save_video(self):
+        self.video_clip.write_videofile(os.path.join(self.save_path, self.save_name))
