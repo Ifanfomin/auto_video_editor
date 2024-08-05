b803df46889974bc575b1d4277dc89ab743c3320
ifanfomin <ifanfomin@rambler.ru>
2024-08-05 21:20:12 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.make_video import MakeVideo
+
+
+def main():
+    MakeVideo("media/video.mp3", "media/audio.mp3")
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/make_video.py
@@ -0,0 +1,5 @@
+class MakeVideo:
+    def __init__(self, video_path, audio_path):
+        self.video_path = video_path
+        self.audio_path = audio_path
+
